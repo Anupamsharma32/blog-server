62716873479689f506b56011c46016dd654d0d48
Anupamsharma32 <109433824+Anupamsharma32@users.noreply.github.com>
2023-11-11 00:32:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const multer = require('multer'); // use to upload
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
-app.use(cors({ credentials: true, origin: 'https://blog-app-anupam.vercel.app' }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
