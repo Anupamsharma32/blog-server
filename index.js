9561a4be5e5d9e94e83af5ea83ced43c5081742b
Anupam <anupamshubhamdeoria@gmail.com>
2023-11-29 23:52:17 +0530

    trycatech added complete

--- index.js
+++ index.js
@@ -89,14 +89,12 @@ app.post('/logout', (req, res) => {
 app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
 
 
-
-   const { originalname, path } = req.file;
-   const parts = originalname.split('.');
-   const ext = parts[parts.length - 1];
-   const newPath = path + '.' + ext;
-   fs.renameSync(path, newPath);
-
    try {
+      const { originalname, path } = req.file;
+      const parts = originalname.split('.');
+      const ext = parts[parts.length - 1];
+      const newPath = path + '.' + ext;
+      fs.renameSync(path, newPath);
       const { token } = req.cookies;
       jwt.verify(token, secret, {}, async (err, info) => {
          if (err) throw err;
@@ -115,7 +113,7 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
 
       res.status(400).send({
          success: false,
-         message: "Error in creating post ",
+         message: "Error in creating post !!!!!!! ",
          error
       })
 
