ecde83e51f7833f423d0e6dc31b7c0934fd5c426
Anupam <anupamshubhamdeoria@gmail.com>
2023-12-12 22:10:55 +0530

    changed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose")
 const app = express();
 const cors = require('cors');
+const bodyParser=require("body-parser")
 const User = require('./models/User');
 const Post = require('./models/Post');
 const bcrypt = require('bcrypt')
@@ -10,9 +11,10 @@ const multer = require('multer'); // use to upload
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
-// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use(cors({ credentials: true, origin: 'https://blog-app-anupam-ewi1p3bff-anupamsharma32s-projects.vercel.app' }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+// app.use(cors({ credentials: true, origin: 'https://blog-app-anupam-ewi1p3bff-anupamsharma32s-projects.vercel.app' }));
 app.use(express.json());
+
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 const salt = bcrypt.genSaltSync(10);// random string
@@ -56,23 +58,39 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-   const { username, password } = req.body;
-   const userDoc = await User.findOne({ username });
-   const passOk = bcrypt.compareSync(password, userDoc.password);
-   if (passOk) {
-      // logged in
-      jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-         if (err) throw err;
-         res.cookie('token', token); // Fix: Use res.cookie on the response object
-         res.json({
-            id: userDoc._id,
-            username,
+   try {
+      const { username, password } = req.body;
+      const userDoc = await User.findOne({ username });
+
+      if (!userDoc) {
+         // User not found
+         return res.status(401).json({ error: 'Invalid username or password' });
+      }
+
+      const passOk = bcrypt.compareSync(password, userDoc.password);
+
+      if (passOk) {
+         // Password is correct
+         jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token);
+             return res.json({
+               id: userDoc._id,
+               username,
+            });
          });
-      });
-   } else {
-      res.status(400).json('wrong credentials');
+      } else {
+         // Password is incorrect
+        return  res.status(401).json({ error: 'Invalid username or password' });
+      }
+
+   } catch (error) {
+      console.error('Error during login:', error);
+      res.status(500).json({ error: 'Internal server error' });
    }
 });
+
+
 
 
 app.get('/profile', (req, res) => {
@@ -85,89 +103,9 @@ app.get('/profile', (req, res) => {
 const { updateOne } = require('mongoose');
 app.post('/logout', (req, res) => {
    res.cookie('token', '').json('ok');
+   res.status(200).json({ message: 'Logout successful' });
+   
 });
-// app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
-//    try {
-//       const { originalname, path } = req.file;
-//       const parts = originalname.split('.');
-//       const ext = parts[parts.length - 1];
-//       const newPath = path + '.' + ext;
-//       fs.renameSync(path, newPath);
-
-//       const { token } = req.cookies;
-//       jwt.verify(token, secret, {}, async (err, info) => {
-//          if (err) {
-//             console.error(err);
-//             return res.status(401).json({ success: false, message: 'Unauthorized' });
-//          }
-
-//          const { title, summary, content } = req.body;
-//          const postDoc = await Post.create({
-//             title,
-//             summary,
-//             content,
-//             cover: newPath,
-//             author: info.id,
-//          });
-//          res.json(postDoc);
-//       });
-
-//    } catch (error) {
-//       console.error(error);
-//       res.status(500).send({
-//          success: false,
-//          message: 'Error in creating post.',
-//          error,
-//       });
-//    }
-// });
-
-// app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
-//    try {
-//       let newPath = null;
-//       if (req.file) {
-//          const { originalname, path } = req.file;
-//          const parts = originalname.split('.');
-//          const ext = parts[parts.length - 1];
-//          newPath = path + '.' + ext;
-//          fs.renameSync(path, newPath);
-//       }
-
-//       const { token } = req.cookies;
-//       jwt.verify(token, secret, {}, async (err, info) => {
-//          if (err) {
-//             console.error(err);
-//             return res.status(401).json({ success: false, message: 'Unauthorized' });
-//          }
-
-//          const { id, title, summary, content } = req.body;
-//          const postDoc = await Post.findById(id);
-
-//          if (!postDoc || JSON.stringify(postDoc.author) !== JSON.stringify(info.id)) {
-//             return res.status(400).json({ success: false, message: 'You are not the author or post not found.' });
-//          }
-
-//          await postDoc.updateOne({
-//             title,
-//             summary,
-//             content,
-//             cover: newPath ? newPath : postDoc.cover,
-//          });
-
-//          res.json(postDoc);
-//       });
-
-//    } catch (error) {
-//       console.error(error);
-//       res.status(500).send({
-//          success: false,
-//          message: 'Error in updating post.',
-//          error,
-//       });
-//    }
-// });
-
-
 app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
    try {
       const { originalname, path } = req.file;
@@ -248,7 +186,6 @@ app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
       });
    }
 });
-// ... Other parts of your code
 
 
 app.get('/post', async (req, res) => {
@@ -256,7 +193,7 @@ app.get('/post', async (req, res) => {
       await Post.find()
          .populate('author', ['username'])
          .sort({ createdAt: -1 })
-         .limit(20)
+         .limit(40)
    );
 });
 
